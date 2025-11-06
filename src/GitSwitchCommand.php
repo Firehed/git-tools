4394d6b327b03fe4806f7b00f26bf56fd4cf75ce
Eric Stern <eric@ericstern.com>
2025-11-06 09:41:10 -0800

    Convert command structure to new attribute-based system

--- src/GitSwitchCommand.php
+++ src/GitSwitchCommand.php
@@ -9,40 +9,27 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Attribute\{
+    AsCommand,
+    Argument,
+};
 
+#[AsCommand(name: 'git:switch', description: 'Interactively switches to a branch')]
 class GitSwitchCommand extends Command
 {
     use CommandTrait;
 
-    private const ARG_BRANCH_INDEX = 'index';
-
-    public function configure(): void
-    {
-        $this->setName('git:switch');
-        $this->setDescription('Interactively switches to a branch');
-        $this->addArgument(
-            name: self::ARG_BRANCH_INDEX,
-            mode: InputArgument::OPTIONAL,
-            description: 'The numeric index of of the branch to switch to',
-        );
-    }
-
-    public function execute(InputInterface $input, OutputInterface $output): int
-    {
+    public function __invoke(
+        #[Argument('The numeric index of the branch to switch to')] ?int $index,
+        InputInterface $input,
+        OutputInterface $output,
+    ): int {
         $currentBranch = $this->repo->getCurrentBranch();
         $defaultBranch = $this->repo->getDefaultBranchName();
         $branches = $this->repo->getSortedBranchNames();
 
-        $index = $input->getArgument(self::ARG_BRANCH_INDEX);
-
         if ($index === null) {
             $index = $this->askForBranchIndex('Switch to which branch?', $branches, $currentBranch, $input, $output);
-        } else {
-            assert(is_string($index));
-            if (!ctype_digit($index)) {
-                throw new RuntimeException('Invalid branch.');
-            }
-            $index = (int) $index;
         }
 
         if ($branches[$index] === $currentBranch) {
